8d5a8ee5e98b92cb7eca1e8dbbfc43e82cc66ef0
Chris Brinker <christopherbrinker@web.de>
2021-12-04 19:59:31 +0100

    Initial commit

---
+++ app/src/main/java/scom/example/geldboerse/MainActivity.java
@@ -0,0 +1,14 @@
+package scom.example.geldboerse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
